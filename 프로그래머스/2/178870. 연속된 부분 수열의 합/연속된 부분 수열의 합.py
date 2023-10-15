4c7b2d6edfa73001315e500ae56957eb8f6ee53a
Hyeongmin Kim <hminkim2@gmail.com>
2023-10-15 19:59:54 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,25 +1,24 @@
 def solution(sequence, k):
     answer = []
-    idx1 = 0
-    idx2 = 0
+    start, end = 0,0
     temp = sequence[0]
-    length = 1000001
+    min_len = 1000001
 
-    while idx1 <= idx2 and idx2 < len(sequence):
+    while start <= end < len(sequence):
         if temp == k :
-            if idx2 - idx1 + 1 < length :
-                length = idx2 - idx1 + 1
-                answer = [idx1, idx2]
-            temp -= sequence[idx1]
-            idx1 += 1
+            if end - start + 1 < min_len :
+                min_len = end - start + 1
+                answer = [start, end]
+            temp -= sequence[start]
+            start += 1
 
         elif temp < k :
-            idx2 += 1
-            if idx2 < len(sequence) :
-                temp += sequence[idx2]
+            end += 1
+            if end < len(sequence) :
+                temp += sequence[end]
 
         elif temp > k :
-            temp -= sequence[idx1]
-            idx1 += 1
+            temp -= sequence[start]
+            start += 1
 
     return answer
